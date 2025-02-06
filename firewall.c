82e74858b8c8d9fa1a91bea5668abe18fcd1bc6c
liam <liamtbo@gmail.com>
2025-02-06 19:29:24 +0000

    finished packet_spoofer.py

--- firewall.c
+++ firewall.c
@@ -10,7 +10,7 @@ insert kernel module into the running kernel
 	sudo insmod firewall.ko
 list the kernel module
 	lsmod | grep firewall
-sudo rmmod kMod
+remove firewall module from kernel (ending firewall)
 	sudo rmmod firewall
 check kernel printout
 	sudo dmesg
@@ -26,7 +26,7 @@ static struct nf_hook_ops hook1, hook2;
 
 unsigned int hello1(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
 	printk(KERN_INFO "*** Hello, netfilter 11111!\n");
-	return NF_ACCEPT;
+	return NF_DROP;
 }
 
 unsigned int hello2(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
@@ -38,16 +38,17 @@ int registerFilter(void) {
 	printk(KERN_INFO "Registering filters.\n");
 
 	hook1.hook = hello1;
-	hook1.hooknum = NF_INET_LOCAL_OUT;
+	hook1.hooknum = NF_INET_LOCAL_IN;
 	hook1.pf = PF_INET;
 	hook1.priority = -100;
 	nf_register_net_hook(&init_net, &hook1);
-
+	/*
 	hook2.hook = hello2;
 	hook2.hooknum = NF_INET_LOCAL_OUT;
 	hook2.pf = PF_INET;
 	hook2.priority = -180;
 	nf_register_net_hook(&init_net, &hook2);
+	*/
 	return 0;
 }
 
---
+++ packet_spoofer.py
@@ -0,0 +1,29 @@
+
+
+from scapy.all import *
+import time
+import random
+
+def main():
+    d = {}
+    d[0] = ("China", "1.0.0.0/8")
+    d[1] = ("Germany", "2.0.0.0/8")
+    d[2] = ("United States", "3.0.0.0/8")
+    d[3] = ("Japan", "133.0.0.0/8")
+
+    while (1):
+        a = IP()
+
+        # create spoofed ip_src
+        src_country, src_ip = d[random.randint(0,3)]
+        rand_host_bytes = str(random.randint(0,255))
+        backslash_i = src_ip.index('/')
+        a.src = src_ip[:backslash_i - 1] + rand_host_bytes
+        print(a.src) 
+        # built packet
+        # b = ICMP()
+        # p = a/b
+        # send it out baby
+        # send(p, verbose=1)
+        time.sleep(2)
+main()
